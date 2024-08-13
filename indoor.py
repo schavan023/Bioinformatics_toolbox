1ddacbcaa39976eddce07e21271852c203bda9cf
Suchitra Chavan <85761952+schavan023@users.noreply.github.com>
2024-08-13 19:03:23 -0400

    Create indoor.py

---
+++ indoor.py
@@ -0,0 +1,6 @@
+# In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase. Punctuation and whitespace should be outputted unchanged. You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.
+# User input is needed.
+input_user = input("Enter your text here:")
+# Uppercase to lower case conversion. Punctutation and whitespace as is.
+lowercase_output = input_user.lower()
+print(lowercase_output)
