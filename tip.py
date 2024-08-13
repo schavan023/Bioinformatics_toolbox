f53cd2bac381e843d74b6e88b418555ddbd57e27
Suchitra Chavan <85761952+schavan023@users.noreply.github.com>
2024-08-13 19:07:08 -0400

    Create tip.py

---
+++ tip.py
@@ -0,0 +1,19 @@
+# implement dollars_to_float, which should accept a str as input (formatted as $##.##, wherein each # is a decimal digit), remove the leading $, and return the amount as a float. For instance, given $50.00 as input, it should return 50.0.
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    # First remove the $ sign
+   return float(d.strip("$"))
+
+
+
+def percent_to_float(p):
+    # Remove the % sign and do calculation of percentage value.
+    return float(p.strip('%')) / 100
+
+main()
